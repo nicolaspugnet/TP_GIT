f25936320114418d783d0a85ef516ce6372ec2c1
Ducros <rducros34@gmail.com>
2021-06-03 16:01:28 +0200

    V6

--- TP_GIT/src/Test.java
+++ TP_GIT/src/Test.java
@@ -4,6 +4,8 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("projet TP_GIT Version 1");
 		System.out.println("projet TP_GIT Version 2");
+		System.out.println("projet TP_GIT Version 3 - RD");
+
 		System.out.println("projet TP_GIT Version 4");
 		System.out.println("projet TP_GIT Version 5");
 	}
